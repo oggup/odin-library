9850db8aed21173c1c66460293cfe532f10b0ee4
Oggup <edifoo@gmail.com>
2022-07-30 22:49:50 -0700

    add div added

--- script.js
+++ script.js
@@ -11,19 +11,27 @@ function addBookToLibrary(book) {
   bookList.push(book);
   let div = document.createElement("div");
   div.classList.add("book-card");
-  div.innerHTML = `<p>Title: ${book.title}</>
-    <p>Author: ${book.author}</>
-    <p>Pages: ${book.pages}</>
-    <p>Date Written: ${book.year}</>
+  div.innerHTML = `<div class="card-title">${book.title}</div>
+  <div class="card-info">
+      <div class="object-property-value"><div class="property">Author:</div><div class="value"> ${book.author}</div></div>
+      <div class="object-property-value"><div class="property">Pages:</div><div class="value"> ${book.pages}</div></div>
+      <div class="object-property-value"><div class="property">Date Written:</div><div class="value"> ${book.year}</div></div>
+  </div>
+  <div class="card-buttons"><button class="">1</button><button class="">2</button><button class="">3</button></div>
     `;
   document.querySelector(".book-container").appendChild(div);
 
   console.log(book.title, book.author, book.pages, book.year);
 
 }
+
+
+
+
+
 addBookToLibrary(rakkBook);
 addBookToLibrary(new Book("Top Ten Legumes", "Papaya", 200, 2004));
 addBookToLibrary(
-  new Book("Sleepless in Monatana: A Memoir", "Aaron Jonson", 782, 2022)
+  new Book("Sleepless in Montana: A Memoir", "Aaron Jonson", 782, 2022)
 );
 
